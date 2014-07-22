f23308da394157f5f47cb504de6ed4229ef89efa
roshanca <wwj1983@gmail.com>
2014-07-22 19:23:24 +0800

    Add risk evaluation module

---
+++ src/js/controllers/riskController.js
@@ -0,0 +1,39 @@
+define(['js/views/riskView', 'GS'], function (View, GS) {
+
+	var bindings = [{
+		element: '#logout',
+		event: 'click',
+		handler: GS.logout
+	}, {
+		element: '.next-button',
+		event: 'click',
+		handler: nextSubmit
+	}];
+
+	function init() {
+		khApp.showIndicator();
+		$$.ajax({
+			url: 'api/question.json',
+			type: 'POST',
+			success: function (data) {
+				var data = JSON.parse(data);
+				if (data.errorNo == '0') {
+					var model = data.model;
+					View.render({
+						bindings: bindings,
+						model: model
+					});
+					khApp.hideIndicator();
+				}
+			}
+		});
+	}
+
+	function nextSubmit() {
+		mainView.loadPage('review.html');
+	}
+
+	return {
+		init: init
+	};
+});
