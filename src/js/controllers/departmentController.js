2d1c8918d1e9c44a4662ceb74f1d4a65cced9815
roshanca <wwj1983@gmail.com>
2014-07-23 16:02:04 +0800

    Improve department module

--- src/js/controllers/departmentController.js
+++ src/js/controllers/departmentController.js
@@ -8,16 +8,55 @@ define(['js/views/departmentView', 'GS'], function (View, GS) {
 		element: '.next-button',
 		event: 'click',
 		handler: nextSubmit
+	}, {
+		element: document,
+		event: 'pageInit',
+		handler: setName
+	}, {
+		element: '.nearby',
+		event: 'click',
+		handler: setTrigger
+	}, {
+		element: '.all',
+		event: 'click',
+		handler: setTrigger
 	}];
 
 	function init() {
-		View.render({
-			bindings: bindings
+		khApp.showIndicator();
+		$$.ajax({
+			url: 'api/department.json',
+			type: 'POST',
+			success: function (data) {
+				var data = JSON.parse(data);
+				if (data.errorNo == '0') {
+					var model = data.model;
+					View.render({
+						bindings: bindings,
+						model: model
+					});
+					khApp.hideIndicator();
+				}
+			}
 		});
 	}
 
 	function nextSubmit() {
 		mainView.loadPage('video.html');
+		$$(document).off('pageInit', setName);
+	}
+
+	function setName(e) {
+		var page = e.detail.page;
+		if (page.from === 'left') {
+			console.log(page);
+			var name = View.getTrigger().text();
+			View.renderName(name);
+		}
+	}
+
+	function setTrigger() {
+		View.setTrigger($$(this));
 	}
 
 	return {
