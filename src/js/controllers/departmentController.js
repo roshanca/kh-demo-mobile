43ab7ab0af3c3abdfd010ca1b7844108c9074c72
roshanca <wwj1983@gmail.com>
2014-08-01 17:52:21 +0800

    Split data transfer from single to multi on department page

--- src/js/controllers/departmentController.js
+++ src/js/controllers/departmentController.js
@@ -5,24 +5,38 @@ define(['js/views/departmentView', 'GS'], function (View, GS) {
 		event: 'click',
 		handler: GS.logout
 	}, {
-		element: '.next-button',
+		element: '.department-next-button',
 		event: 'click',
 		handler: nextSubmit
 	}, {
-		element: document,
-		event: 'pageInit',
-		handler: setName
-	}, {
-		element: '.nearby',
+		element: '.nearby-link',
 		event: 'click',
-		handler: setTrigger
+		handler: selectFromNearby
 	}, {
-		element: '.all',
+		element: '.all-link',
 		event: 'click',
-		handler: setTrigger
+		handler: selectFromAll
+	}];
+
+	var afterBindings = [{
+		element: '[name=nearby]',
+		event: 'change',
+		handler: selectedName
+	}, {
+		element: '[name=all]',
+		event: 'change',
+		handler: selectedName
 	}];
 
 	function init() {
+		View.render({
+			bindings: bindings
+		});
+
+		laodRecDepartment();
+	}
+
+	function laodRecDepartment() {
 		khApp.showIndicator();
 		$$.ajax({
 			url: 'api/department.json',
@@ -30,33 +44,61 @@ define(['js/views/departmentView', 'GS'], function (View, GS) {
 			success: function (data) {
 				data = JSON.parse(data);
 				if (data.errorNo === 0) {
-					var model = data.model;
-					View.render({
-						bindings: bindings,
-						model: model
-					});
+					View.renderName(data.model.recDepartment);
 					khApp.hideIndicator();
 				}
 			}
 		});
 	}
 
-	function nextSubmit() {
-		mainView.loadPage('video.html');
-		$$(document).off('pageInit', setName);
+	function selectFromNearby() {
+		startSelect('nearbySelect', 'api/department_near.json');
 	}
 
-	function setName(e) {
-		var page = e.detail.page;
-		if (page.view.url === 'department.html') {
-			console.log(page.view.url);
-			var name = View.getTrigger().text();
-			View.renderName(name);
+	function selectFromAll() {
+		startSelect('allSelect', 'api/department_all.json');
+	}
+
+	function startSelect(id, url) {
+		var isSelectLocal = $$('#' + id)[0];
+		if (isSelectLocal) {
+			openSelect(id);
+		} else {
+			remoteSelect(id, url);
 		}
 	}
 
-	function setTrigger() {
-		View.setTrigger($$(this));
+	function remoteSelect(id, url) {
+		var _id = id;
+		khApp.showIndicator();
+		$$.ajax({
+			url: url,
+			type: 'POST',
+			success: function (data) {
+				data = JSON.parse(data);
+				if (data.errorNo === 0) {
+					View.renderSelect({
+						bindings: afterBindings,
+						model: data.model
+					});
+					khApp.hideIndicator();
+					openSelect(_id);
+				}
+			}
+		});
+	}
+
+	function openSelect(id) {
+		khApp.smartSelectOpen('#' + id);
+	}
+
+	function selectedName() {
+		var selectedText = this.options[this.selectedIndex].text;
+		View.renderName(selectedText);
+	}
+
+	function nextSubmit() {
+		mainView.loadPage('video.html');
 	}
 
 	return {
--- src/js/views/departmentView.js
+++ src/js/views/departmentView.js
@@ -1,29 +1,22 @@
 define(['utils', 'hbs!js/templates/department'], function (Utils, departmentTemplate) {
 
 	function render(params) {
+		Utils.bindEvents(params.bindings);
+	}
+
+	function renderSelect(params) {
 		var template = departmentTemplate({model: params.model});
 		$$('#departmentContent').append(template);
 		Utils.bindEvents(params.bindings);
 	}
 
-	function setTrigger(el) {
-		$$('#departmentContent').find('.trigger').removeClass('trigger');
-		el.addClass('trigger');
-	}
-
-	function getTrigger() {
-		var trigger = $$('#departmentContent').find('.trigger').find('.item-after');
-		return trigger;
-	}
-
-	function renderName(val) {
-		$$('.department-name').find('h3').text(val);
+	function renderName(text) {
+		$$('.department-name').find('h2').text(text);
 	}
 
 	return {
 		render: render,
-		setTrigger: setTrigger,
-		getTrigger: getTrigger,
-		renderName: renderName
+		renderName: renderName,
+		renderSelect: renderSelect
 	};
 });
