3519700f93190a59d7957bfb6d9cf0ffde83bdc9
roshanca <wwj1983@gmail.com>
2014-08-08 11:40:23 +0800

    Use checkbox instead of switch in password page

--- src/js/views/passwordView.js
+++ src/js/views/passwordView.js
@@ -6,29 +6,10 @@ define(['utils'], function (Utils) {
 	}
 
 	function toggleInputs(visiable) {
-		var wrapper = $$('.sync-trigger').parent(),
-			trigger = $$('.sync-trigger'),
-			content = $$('.sync-item').css('overflow', 'hidden').find('.item-content'),
-			height = trigger.outerHeight(),
-			clientHeight;
-
 		if (visiable === 'hide') {
-			wrapper.css('height', height * 3 + 'px');
-			clientHeight = wrapper[0].clientHeight;
-			wrapper.css('height', height + 'px').transition('300ms');
-			content.transform('translate3d(0,-200%,0)')
-				.transition('300ms')
-				.transitionEnd(function () {
-					trigger.addClass('bare');
-					content.hide();
-				});
+			$$('#sync').hide();
 		} else if (visiable === 'show') {
-			content.show();
-			trigger.removeClass('bare');
-			wrapper.css('height', height + 'px');
-			clientHeight = wrapper[0].clientHeight;
-			wrapper.css('height', height * 3 + 'px').transition('300ms');
-			content.transform('translate3d(0,0,0)').transition('300ms');
+			$$('#sync').show();
 		}
 	}
 
