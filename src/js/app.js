4e5659aa16e0ebd3986fb954142877111869f4c3
roshanca <wwj1983@gmail.com>
2014-08-14 16:28:40 +0800

    Android was won, i have to treat it separately

--- src/js/app.js
+++ src/js/app.js
@@ -17,7 +17,9 @@ require.config({
 require(['Framework7', 'router'], function (Framework7, Router) {
 	window.$$ = Framework7.$;
 
-	window.khApp = new Framework7({
+	var _app = new Framework7();
+
+	var app_elegant = new Framework7({
 		pushState: false,
 		swipeBackPage: false,
 		popupCloseByOutside: false,
@@ -29,6 +31,24 @@ require(['Framework7', 'router'], function (Framework7, Router) {
 		smartSelectBackText: '完成',
 		smartSelectBackTemplate: '<div class="left sliding"><a href="#" class="back link"><i class="icon icon-back-black"></i><span>{{backText}}</span></a></div>'
 	});
+
+	var app_basic = new Framework7({
+		pushState: false,
+		animatePages: false,
+		swipeBackPage: false,
+		swipeout: false,
+		sortable: false,
+		popupCloseByOutside: false,
+		preloadPreviousPage: false,
+		animateNavBackIcon: true,
+		modalTitle: '系统消息',
+		modalButtonOk: '确定',
+		modalButtonCancel: '取消',
+		smartSelectBackText: '完成',
+		smartSelectBackTemplate: '<div class="left sliding"><a href="#" class="back link"><i class="icon icon-back-black"></i><span>{{backText}}</span></a></div>'
+	});
+
+	window.khApp = (_app.device.os === 'android') ? app_basic : app_elegant;
 
 	window.mainView = khApp.addView('#mainView', {
 		dynamicNavbar: true
