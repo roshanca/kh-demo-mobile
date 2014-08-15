47adaf28304435c0f3a3012f510e5702238143f2
roshanca <wwj1983@gmail.com>
2014-08-15 11:05:36 +0800

    Make only one instance of Framework7, otherwise may cause problem

--- src/js/app.js
+++ src/js/app.js
@@ -17,38 +17,40 @@ require.config({
 require(['Framework7', 'router'], function (Framework7, Router) {
 	window.$$ = Framework7.$;
 
-	var _app = new Framework7();
+	var device = Framework7.prototype.device;
 
-	var app_elegant = new Framework7({
-		pushState: false,
-		swipeBackPage: false,
-		popupCloseByOutside: false,
-		preloadPreviousPage: false,
-		animateNavBackIcon: true,
-		modalTitle: '系统消息',
-		modalButtonOk: '确定',
-		modalButtonCancel: '取消',
-		smartSelectBackText: '完成',
-		smartSelectBackTemplate: '<div class="left sliding"><a href="#" class="back link"><i class="icon icon-back-black"></i><span>{{backText}}</span></a></div>'
-	});
-
-	var app_basic = new Framework7({
-		pushState: false,
-		animatePages: false,
-		swipeBackPage: false,
-		swipeout: false,
-		sortable: false,
-		popupCloseByOutside: false,
-		preloadPreviousPage: false,
-		animateNavBackIcon: true,
-		modalTitle: '系统消息',
-		modalButtonOk: '确定',
-		modalButtonCancel: '取消',
-		smartSelectBackText: '完成',
-		smartSelectBackTemplate: '<div class="left sliding"><a href="#" class="back link"><i class="icon icon-back-black"></i><span>{{backText}}</span></a></div>'
-	});
-
-	window.khApp = (_app.device.os === 'android') ? app_basic : app_elegant;
+	if (device.android) {
+		window.khApp = new Framework7({
+			animatePages: false,
+			swipeout: false,
+			sortable: false,
+			cache: false,
+			pushState: false,
+			swipeBackPage: false,
+			popupCloseByOutside: false,
+			preloadPreviousPage: false,
+			animateNavBackIcon: true,
+			modalTitle: '系统消息',
+			modalButtonOk: '确定',
+			modalButtonCancel: '取消',
+			smartSelectBackText: '完成',
+			smartSelectBackTemplate: '<div class="left sliding"><a href="#" class="back link"><i class="icon icon-back-black"></i><span>{{backText}}</span></a></div>'
+		});
+	} else {
+		window.khApp = new Framework7({
+			cache: false,
+			pushState: false,
+			swipeBackPage: false,
+			popupCloseByOutside: false,
+			preloadPreviousPage: false,
+			animateNavBackIcon: true,
+			modalTitle: '系统消息',
+			modalButtonOk: '确定',
+			modalButtonCancel: '取消',
+			smartSelectBackText: '完成',
+			smartSelectBackTemplate: '<div class="left sliding"><a href="#" class="back link"><i class="icon icon-back-black"></i><span>完成</span></a></div>'
+		});
+	}
 
 	window.mainView = khApp.addView('#mainView', {
 		dynamicNavbar: true
