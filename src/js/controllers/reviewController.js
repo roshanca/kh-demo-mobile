c92defe0db685e6e89ce17e5ba1aa453bf56d4c7
roshanca <wwj1983@gmail.com>
2014-08-15 11:09:05 +0800

    Improve review module

--- src/js/controllers/reviewController.js
+++ src/js/controllers/reviewController.js
@@ -39,14 +39,56 @@ define(['views/reviewView', 'GS'], function (View, GS) {
 	}
 
 	function nextSubmit() {
+		var checked = checkDefault();
+
+		if (checked.length > 0) {
+			khApp.alert(checked[0].warn);
+			return;
+		}
+
 		var resultData = khApp.formToJSON('#reviewContent');
+		var resultArray = [];
+		var result;
+		for (var i in resultData) {
+			var _result = i + '&' + resultData[i];
+			resultArray.push(_result);
+		}
 		console.log(resultData);
+
+		result = resultArray.join('|');
+		console.log(result);
 		mainView.loadPage('audit.html');
 	}
 
+	/**
+	 * 点击跟随选择
+	 * @return {void}
+	 */
 	function selectTrigger() {
-		var radio = $$(this).parent().find('[type=radio]');
+		var radio = $$(this).parent().find('input[type=radio]');
+		var value = radio[0].value;
+
 		radio[0].checked = true;
+	}
+
+	/**
+	 * 检查选择是否为默认
+	 * @return {Array} 返回错误序列与错误信息
+	 */
+	function checkDefault() {
+		var result = [];
+
+		$$('.default').each(function (index) {
+			if (!this.checked) {
+				var warn = $$(this).data('warn');
+				result.push({
+					index: index + 1,
+					warn: warn
+				});
+			}
+		});
+
+		return result;
 	}
 
 	return {
