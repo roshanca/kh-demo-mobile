d39985084fd521d85622707402e207dea33809dc
roshanca <wwj1983@gmail.com>
2014-09-04 19:56:33 +0800

    Use gulpjs replace gruntjs

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-#kh-demo-mobile
+# 网上开户移动版 Demo
 
-[![Build Status](http://img.shields.io/travis/roshanca/kh-demo-mobile.svg?style=flat)](https://travis-ci.org/roshanca/kh-demo-mobile)
-[![devDependency Status](http://img.shields.io/david/dev/roshanca/kh-demo-mobile.svg?style=flat)](https://david-dm.org/roshanca/kh-demo-mobile#info=devDependencies)
-[![Built with Grunt](https://dl.dropboxusercontent.com/u/58390955/mybuiltwith-alt-01.svg)](http://gruntjs.com/)
+[![Build Status](http://img.shields.io/travis/roshanca/kh-demo-mobile.svg?style=square)](https://travis-ci.org/roshanca/kh-demo-mobile)
+[![devDependency Status](http://img.shields.io/david/dev/roshanca/kh-demo-mobile.svg?style=square)](https://david-dm.org/roshanca/kh-demo-mobile#info=devDependencies)
 
-## Dependencies
+## 框架
 
 * [Framework7](http://www.idangero.us/framework7/)
-* [requirejs](http://requirejs.org)
-* [requirejs-text](https://github.com/requirejs/text)
-* [mustache](http://mustache.github.io)
+
+`Framework7` 是一套开源框架，用于快速创建基于混合移动应用(hybrid mobile apps, 一般指嵌套在系统 `webView` 容器内的 `HTML5` 的 Web 应用)，其优雅的原生 JS 实现与对 iOS 系统优良界面与交互的模拟，是我选择它作为项目框架的主要原因。甚至我做了些许的 hack，用它创建了相同的 Android App.
+
+## 开发
+
+为了使开发快速高效，使用了以下技术（作为前端，应该不会陌生）：
+
+* 样式预编译器：[LESS](http://www.lesscss.net)
+* 模板引擎：[mustache](http://mustache.github.io)
+* JS 模块依赖管理：[requirejs](http://requirejs.org)
+* 任务流程工具：[gulpjs](http://gulpjs.com)
+
+## 许可
+Copyright (c) 2014 Roshan Wu. GPL v2 Licensed, see [LICENSE](https://github.com/roshanca/kh-demo-mobile/blob/master/LICENSE) for details.
---
+++ gulpfile.js
@@ -0,0 +1,166 @@
+/* ===============================================================================
+************   Import   ************
+=============================================================================== */
+
+var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plugins = gulpLoadPlugins();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var stylish = require('jshint-stylish');
+var runSequence = require('run-sequence');
+
+/* ===============================================================================
+************   Config   ************
+=============================================================================== */
+
+var serveConfig = {
+	files: [
+		'src/**/*.html'
+	],
+	server: {
+		baseDir: 'src/'
+	}
+};
+
+var requireConfig = {
+	baseUrl: 'src/js',
+	mainConfigFile: 'src/js/app.js',
+	name: 'app',
+	paths: {
+		'templates': '../template'
+	},
+	include: [
+		'controllers/accountController',
+		'controllers/appointController',
+		'controllers/auditController',
+		'controllers/certController',
+		'controllers/collectController',
+		'controllers/departmentController',
+		'controllers/depositoryController',
+		'controllers/guideController',
+		'controllers/loginController',
+		'controllers/passwordController',
+		'controllers/profileController',
+		'controllers/protocalController',
+		'controllers/reviewController',
+		'controllers/riskController',
+		'controllers/videoController'
+	],
+	out: 'app.js'
+};
+
+/* ===============================================================================
+************   Task   ************
+=============================================================================== */
+
+gulp.task('browser-sync', function () {
+	browserSync(serveConfig);
+});
+
+gulp.task('less', function () {
+	return gulp.src('src/less/app.less')
+		.pipe(plugins.less())
+		.pipe(gulp.dest('src/css'))
+		.pipe(reload({stream:true}));
+});
+
+gulp.task('lint', function () {
+	return gulp.src(['src/js/**/*.js', '!src/js/libs/*.js'])
+		.pipe(plugins.jshint())
+		.pipe(plugins.jshint.reporter(stylish));
+});
+
+gulp.task('uglify', ['template'], function () {
+	plugins.requirejs(requireConfig)
+		.pipe(plugins.uglify())
+		.pipe(plugins.rename({extname: '.min.js'}))
+		.pipe(gulp.dest('build/js'));
+});
+
+gulp.task('requirejs', function () {
+	return gulp.src('src/js/libs/require.js')
+		.pipe(plugins.uglify())
+		.pipe(plugins.rename({extname: '.min.js'}))
+		.pipe(gulp.dest('build/js'));
+});
+
+gulp.task('replace', function () {
+	return gulp.src('src/index.html')
+		.pipe(plugins.htmlReplace({
+			js: {
+				src: [['js/app.min', 'js/require.min.js']],
+				tpl: '<script data-main="%s" src="%s"></script>'
+			},
+			css: 'css/app.min.css'
+		}))
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('htmlmin', function () {
+	return gulp.src(['src/**/*.html', '!src/index.html'])
+		.pipe(plugins.minifyHtml({empty: true}))
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('template', function () {
+	return gulp.src('src/mustache/*')
+		.pipe(plugins.minifyHtml({empty: true}))
+		.pipe(gulp.dest('src/template'));
+});
+
+gulp.task('cssmin', function () {
+	return gulp.src('src/css/app.css')
+		.pipe(plugins.minifyCss())
+		.pipe(plugins.rename({extname: '.min.css'}))
+		.pipe(gulp.dest('build/css'));
+});
+
+gulp.task('imagemin', function () {
+	return gulp.src('src/img/*')
+		.pipe(plugins.imagemin())
+		.pipe(gulp.dest('build/img'));
+});
+
+gulp.task('copy', function () {
+	return gulp.src(['src/api/*', 'src/font/*'], {base: './src'})
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function () {
+	return gulp.src('build/*', {read: false}).pipe(plugins.clean());
+});
+
+/**
+ * Default task, for developing
+ * @return {[type]} [description]
+ */
+gulp.task('default', ['less', 'browser-sync'], function () {
+	gulp.watch('src/less/*.less', ['less']);
+});
+
+/**
+ * Build task
+ */
+gulp.task('compile', [
+	'less',
+	'requirejs'
+]);
+
+gulp.task('compress', [
+	'htmlmin',
+	'cssmin',
+	'uglify',
+	'imagemin'
+]);
+
+gulp.task('rest', [
+	'copy',
+	'replace'
+]);
+
+gulp.task('build', function () {
+	runSequence('clean', 'compile', 'compress', 'rest', function () {
+		console.log('All done!');
+	});
+});
